43d378c521474d8afef40a3f09b6508b3a7a9077
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-07-15 23:14:36 +0300

    добавлена функция фикса меню при скролле и подсветки текущего пункта

--- js/navigation.js
+++ js/navigation.js
@@ -10,13 +10,31 @@ $(document).ready(function () {
 	});
 });
 
-// фикс меню при скролле
+// фикс меню при скролле и подсветка текущего пункта
 $(document).ready(function() {
 	let nav = $('.nav-container');
 	let nav_bg = $('.nav-scroll-bg');
-	let height = $(".nav-container").height()
+	let height = nav.outerHeight();
+	let sections = $('section');
+	let header = $('#main')
+
+	sections.push(header[0])
  
 	$(window).scroll(function () {
+		// подсветка активного пункта меню при скролле
+		let cur_pos = $(this).scrollTop();
+		
+	  sections.each(function() {
+	    let top = $(this).offset().top - height;
+			let bottom = top + $(this).outerHeight();
+			
+	    if (cur_pos >= top && cur_pos <= bottom) {
+	      nav.find('a').removeClass('active');
+	      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    }
+	  });
+
+		// фикс меню при скролле
 		if ($(this).scrollTop() > 1) {
 			nav.addClass("nav-container--fixed");
 			nav_bg.addClass("nav-scroll-bg--visible");
@@ -27,34 +45,3 @@ $(document).ready(function() {
 		}
 	});
 });
-
-
-// подсветка активного пункта меню при скролле
-// var sections = $('section'), nav = $('.nav-container'), nav_height = nav.outerHeight();
-
-// $(window).on('scroll', function () {
-//   var cur_pos = $(this).scrollTop();
-  
-//   sections.each(function() {
-//     var top = $(this).offset().top - nav_height,
-//         bottom = top + $(this).outerHeight();
-    
-//     if (cur_pos >= top && cur_pos <= bottom) {
-//       nav.find('a').removeClass('active');
-//       sections.removeClass('active');
-      
-//       $(this).addClass('active');
-//       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-//     }
-//   });
-// });
-
-// nav.find('a').on('click', function () {
-//   var $el = $(this), id = $el.attr('href');
-  
-//   $('html, body').animate({
-//     scrollTop: $(id).offset().top - nav_height
-//   }, 500);
-  
-//   return false;
-// });
