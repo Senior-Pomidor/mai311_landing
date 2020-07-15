4ed0fdca7ccba5f5597b2baaa1319dbfc17ce34a
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-07-15 14:48:00 +0300

    добавлена функция фикса меню при скролле

---
+++ js/navigation.js
@@ -0,0 +1,29 @@
+// плавный скролл
+$(document).ready(function () {
+	$("#menu").on("click", "a", function (event) {
+		event.preventDefault();
+		let id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({
+			scrollTop: top
+		}, 500);
+	});
+});
+
+// фикс меню при скролле
+$(document).ready(function() {
+	let nav = $('.nav-container');
+	let nav_bg = $('.nav-scroll-bg');
+	let height = $(".nav-container").height()
+ 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1) {
+			nav.addClass("nav-container--fixed");
+			nav_bg.addClass("nav-scroll-bg--visible");
+			nav_bg.height(height);
+		} else {
+			nav.removeClass("nav-container--fixed");
+			nav_bg.removeClass("nav-scroll-bg--visible");
+		}
+	});
+});
